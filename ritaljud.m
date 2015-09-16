1a4afe3356f443052fa603efa6ec0ad2934239f8
Falden <aldenfanny@gmail.com>
2015-09-16 16:35:49 +0200

    tar bort filer

--- ritaljud.m
+++ ritaljud.m
@@ -1,14 +1,14 @@
 %%Ritaljud 
-
+close all
 clc, clear
 
 %dBFS Ändrar amplituden tror jag. 
 
-[y,fs]=audioread('2000Hz.wav');
+[y,fs]=audioread('2000Hz-6.wav');
 
 y = y(:,1);             % get the first channel
 ymax = max(abs(y));     % find the maximum value
-ya = y/ymax;             % scalling the signal
+ya = y/ymax;            % scalling the signal
 
 ydft = fft(y); %fft to transform the original signal into frequency domain
 n = length (y); %length of the original signal
@@ -48,6 +48,28 @@ grid on;
 %gångra med.
 radius= ymax*1000;
 
+for freq = (0:fs/length(y):fs/2);
+if freq>=500 && freq<1000
+    color = '.k'; %Vet dock inte hur man får en string to string eller vad det blir...
+
+elseif freq>=1000 && freq<1500
+    color = '.r'; 
+
+elseif freq>=1500 && freq<2000
+    color = '.g'; 
+
+elseif freq>=2000 && freq<2500
+    color = '.b';
+
+else
+    color = '.m'; %random färg för att se om det funkar (magenta). 
+    end
+end  
+figure;
+%Plotta en fylld cirkel
+plot(1, 1, color, 'MarkerSize', radius) %(1,1) avgör koordinaten. 
+
+freq
 %Frekvens färg:
 
 %500-1000 Svart '.k'
@@ -62,7 +84,7 @@ radius= ymax*1000;
 %%
 
 %Testar att sätta frekvensen till ett värde
-frekvens= 1200;
+frekvens= 1600;
 
 
 %Och här nedan försökte jag få en viss färg på pricken beroende på
@@ -70,30 +92,26 @@ frekvens= 1200;
 
 if frekvens>=500 && frekvens<1000
     color = '.k' %Vet dock inte hur man får en string to string eller vad det blir...
-end
 
-if frekvens>=1000 && frekvens<1500
-    '.r' = color; 
-end
+elseif frekvens>=1000 && frekvens<1500
+    color = '.r'; 
 
-if frekvens>=1500 && frekvens<2000
-    '.g' = color; 
-end
+elseif frekvens>=1500 && frekvens<2000
+    color = '.g'; 
 
-if frekvens>=2000 && frekvens<2000
-    '.b' = color; 
-end
+elseif frekvens>=2000 && frekvens<2500
+    color = '.b';
 
 else
-    '.m' = color; %random färg för att se om det funkar (magenta). 
+    color = '.m'; %random färg för att se om det funkar (magenta). 
+    end
     
-%%    
 figure;
 %Plotta en svart fylld cirkel
-%(1, 1, color, 'MarkerSize', radius) %(1,1) avgör koordinaten. 
+plot(1, 1, color, 'MarkerSize', radius) %(1,1) avgör koordinaten. 
 
 %Såhär skriver man för att få en röd cirkel:
-plot(1, 1, '.r', 'MarkerSize', radius)
+%plot(1, 1, '.r', 'MarkerSize', radius)
 
 
 
