a240688853b006861cc9005c4e58b026d3105b3d
Falden <aldenfanny@gmail.com>
2015-09-28 16:08:57 +0200

    bortagna filer

--- plotspectro.m
+++ plotspectro.m
@@ -1,5 +1,5 @@
 
-clear, clc, close all
+clear, close all
 
 % get a section of the sound file
 [x,fs] = audioread('1000Hz.wav');
@@ -15,6 +15,7 @@ t = (0:N-1)/fs;
 
 % plotting of the spectrogram
 figure;
+
 spectrogram(x, 1024, 3/4*1024, [], fs, 'yaxis')
 
 h = colorbar;    %gör en colorbat   
